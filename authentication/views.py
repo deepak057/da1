ece510f908ac8c6ca89322becba02c9704d9b9d8
deepak <deepak057@yahoo.com>
2017-03-26 23:06:52 +0530

    all the new changes

--- authentication/views.py
+++ authentication/views.py
@@ -2,3 +2,5 @@ from rest_framework import permissions, viewsets
 from authentication.models import Account
 from authentication.permissions import IsAccountOwner
 from authentication.serializers import AccountSerializer
+
+class AccountViewSet(viewsets.ModelViewSet):
